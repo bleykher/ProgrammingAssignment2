d6e70ec499e9c058bb2b9b1e2e3304b011100826
Alexander Bleykher <bleykher@gmail.com>
2014-12-20 02:16:19 -0800

    Implementation for caching the inverse of a matrix assignment (Programm assignment #2)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions are used to create a specical object
+## that stores a matrix and caches its inverse matrix
+## (matrix inversion is usually a costly computation)
 
-## Write a short comment describing this function
-
+## The function creates special matrix" which is a list containing the following functions:
+## set value of the matrix, get value of the matrix
+## set value of the inverse matrix, get value of the inverse matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL # cache for inverse matrix
+        
+        # set function for main matrix
+        set <- function(y) {
+                x <<- y
+                i <<- NULL # if we reinitialize matrix we should drop cache
+        }
+        
+        # get value of main matrix
+        get <- function() x
+        
+        # set value of inverse matrix (cache)
+        setinverse <- function(inverse) i <<- inverse
+        
+        # get value of inverse matrix (from cache)
+        getinverse <- function() i
+        
+        #return list with all functions
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## The function calculates inverse matrix for special "matrix"
+## created by makeCacheMatrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # read data from cache
+        i <- x$getinverse()
+        if(!is.null(i)) { # if cache data is not null then use cache
+                message("getting cached data")
+                return(i)
+        }
+        
+        data <- x$get() # get main matrix
+        i <- solve(data, ...) # calculate inverse matrix
+        x$setinverse(i) # set cache
+        i #return inverse matrix
 }
